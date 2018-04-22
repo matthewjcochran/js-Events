6a65ad4f65ae554e297ccdb1345f222df634b037
Matthew Cochran <matthewjcochran@gmail.com>
2018-04-22 12:48:23 -1000

    assignment done

--- js/app.js
+++ js/app.js
@@ -54,9 +54,7 @@ function redFace() {
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
 function showPrice() {
-    var dollars =  document.createElement('p');
-    dollars.innerHTML = '$5.55';
-    price.appendChild(dollars);
+    price.innerHTML = '$5.55';
 }
 
 //7. Mr. Buttons
@@ -64,14 +62,46 @@ function showPrice() {
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+Benjamin.addEventListener('click', addQuote);
+
+function addQuote() {
+    displayQuote.innerHTML = myQuote;
+}
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+random.addEventListener('click', addRandom);
 
+function addRandom() {
+    var i = Math.floor(Math.random()*quotes.length);
+    displayQuotes.innerHTML = quotes[i];
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+
+showHide.addEventListener('click', showOrHide);
+
+function showOrHide() {
+    if(showmoney.style.display !== 'inline'){
+        showmoney.style.display = 'inline';
+    }else{
+        showmoney.style.display = 'none';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
